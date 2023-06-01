849d423bc2bcbdccdc445dd6521fee383f6c3478
karton675 <127328589+karton675@users.noreply.github.com>
2023-06-01 16:33:03 +0000

    6_2

--- Main.java
+++ Main.java
@@ -2,16 +2,17 @@ import java.io.IOException;
 import java.util.Scanner;
 
 
+
 class WrongStudentName extends Exception { }
-class WrongDateOfBirth extends Exception { }
-class WrongAge extends Exception { }
+class WrongAge extends Exception{ }
+class WrongMenu extends Exception{}
 class Main {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         while(true) {
             try {
-                int ex = menu();
+                int ex = ReadMenu();
                 switch(ex) {
                     case 1: exercise1(); break;
                     case 2: exercise2(); break;
@@ -20,23 +21,36 @@ class Main {
                 }
             } catch(IOException e) {
 
-            }catch(WrongStudentName e) {
+            } catch(WrongStudentName e) {
                 System.out.println("Błędne imie studenta!");
-            }catch(WrongDateOfBirth e) {
-                System.out.println("Błędna data urodzenia!");
-            }catch(WrongAge e) {
-                System.out.println("Błędny wiek!");
+            } catch(WrongAge e) {
+              System.out.println("Błędny Wiek!");
+              }
+          catch(WrongMenu e){
+              System.out.println("Podaj numer z menu!");
+
+            
             }
-       }
+        }
     }
 
-    public static int menu() {
+    public static int ReadMenu() throws WrongMenu {
+      int result;
         System.out.println("Wciśnij:");
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
         System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
+        try {
+      result = scan.nextInt();
+    } catch (Exception e) {
+      scan.nextLine();
+      throw new WrongMenu();
+    }
+    if (result < 0 || result > 3) {
+      throw new WrongMenu();
+    }
+    return result;
     }
 
     public static String ReadName() throws WrongStudentName {
@@ -48,31 +62,27 @@ class Main {
 
         return name;
     }
-    public static int ReadAge() throws WrongAge {
-        
+      public static int ReadAge() throws WrongAge{
         System.out.println("Podaj wiek: ");
         int age = scan.nextInt();
-        if( age >=100 || age <=0 )
-            throw new WrongAge();
-
+        if(age <= 0 || age>100)
+          throw new WrongAge ();
         return age;
-    }
-   
-
-
+    
+      }
+      
+    
   
-   
 
-    public static void exercise1() throws IOException, WrongStudentName, WrongAge, WrongDateOfBirth {
+    public static void exercise1() throws IOException, WrongStudentName,WrongAge {
         var name = ReadName();
         var age = ReadAge();
-        
-      
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
         var date = scan.nextLine();
         (new Service()).addStudent(new Student(name, age, date));
     }
+  
 
     public static void exercise2() throws IOException {
         var students = (new Service()).getStudents();
